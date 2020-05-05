a6dd3d6e09d89b464ac5c5516c86107a26b71b8a
Anttonertmaphosa <62391300+Anttonertmaphosa@users.noreply.github.com>
2020-05-05 18:29:12 +0200

    Add files via upload
    added testing

--- spec/person_spec.js
+++ spec/person_spec.js
@@ -1,37 +1,30 @@
-const { person } = require("../src/person");
+const { Person } = require("../src/person.js");
+let personObject = new Person("Ryan", 30, "male", [
+  " being a hardarse",
+  " agile",
+  " and ssd hard drives.",
+]);
 
-describe("checks name", function() {
-    it("check if person.name is Ryan", function() {
-        expect(person.name).toBe("Ryan")
-    })
-})
-
-describe("checks age", function(){
-    it("check if person.age is 30", function(){
-        expect(person.age).toBe(30)
-    })
-})
-
-describe("checks gender", function(){
-    it("check if person.gender is male", function(){
-        expect(person.gender).toBe("male")
-    })
-})
-
-describe("checks first interest ", function(){
-    it("check if person.interests[0] is being a hardarse", function(){
-        expect(person.interests[0]).toBe("being a hardarse")
-    })
-})
-
-describe("checks second interest ", function(){
-    it("check if person.interests[1] is agile", function(){
-        expect(person.interests[1]).toBe("agile")
-    })
-})
-
-describe("checks third interest ", function(){
-    it("check if person.interests[2] is ssd hard drives", function(){
-        expect(person.interests[2]).toBe("ssd hard drives")
-    })
-})
+describe("The Person class is being described", function () {
+  it("return the name property of the person object", function () {
+    expect(personObject.name).toBe("Ryan");
+  });
+  it("return the age property of the person object", function () {
+    expect(personObject.age).toEqual(30);
+  });
+  it("return the gender property of the person object", function () {
+    expect(personObject.gender).toBe("male");
+  });
+  it("return an array of interest from the person object", function () {
+    expect(personObject.interests).toEqual([
+      " being a hardarse",
+      " agile",
+      " and ssd hard drives.",
+    ]);
+  });
+  it("return the full content of greeting including the new Person object person and hello() method", function () {
+    expect(personObject.hello()).toBe(
+      "Hello, my name is Ryan and I am 30years old. My interests are being a hardarse, agile, and ssd hard drives."
+    );
+  });
+});
--- src/person.js
+++ src/person.js
@@ -1,17 +1,32 @@
 class Person {
-    constructor (name,age,gender,interests){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.interests = interests;
-    } 
+  //created a constructor and added properties
+  constructor(name, age, gender, interests) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.interests = interests;
+  }
+  //Creating a Method
+  hello() {
+    return (
+      "Hello, my name is " +
+      this.name +
+      " and I am " +
+      this.age +
+      "years old. " +
+      "My interests are" +
+      this.interests
+    );
+  }
+}
+//creating new objects that are link to person class
+let person = new Person("Ryan", 30, "male", [
+  " being a hardarse",
+  " agile",
+  " and ssd hard drives.",
+]);
+let greeting = person.hello();
 
-    hello() {
-        return `Hello, my name is ${this.name} and I am  ${this.age} years old. My interests are ${this.interests[0]},${this.interests[1]} and ${this.interests[2]}`
-    }
+console.log(greeting);
 
-};
-
-let person = new Person('Ryan',30,'male',['being a hardarse', 'agile', 'ssd hard drives'] );
-
-module.exports = { person }
+module.exports = { Person };
